50915385794ae600d6cc821efca6a886eda4c129
zeotrix <mosy.irani@gmail.com>
2025-03-15 22:45:33 +0330

    fe: every currency has link

--- crypto/views.py
+++ crypto/views.py
@@ -5,4 +5,7 @@ def home(request):
     with connection.cursor() as cursor:
         cursor.execute("SELECT nobitex_currencies, wallex_currencies, common_currencies FROM prices")
         data = cursor.fetchall()
-    return render(request, 'home.html', {'data': data})
+    base_url = "https://www.binance.com/en/trade/"
+    nobitex_url = "https://nobitex.ir/panel/exchange/"
+    wallex_url = "https://wallex.ir/app/trade/"
+    return render(request, 'home.html', {'data': data, 'base_url': base_url, 'nobitex_url': nobitex_url, 'wallex_url': wallex_url})
